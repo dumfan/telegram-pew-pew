0b652b9d9543e9f4f9dfe1484c644c0fe42365fb
Max Malm <benjick@dumfan.net>
2016-06-23 12:06:18 +0200

    Added Docker support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const log = require('./log');
+
+if (!process.env.TELEGRAM_TOKEN) {
+  log.warn('TELEGRAM_TOKEN is not defined');
+  process.exit();
+}
+
 const { loadCommands } = require('./helpers');
 const listen = require('./listener');
 
